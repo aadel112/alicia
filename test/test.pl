48d6d9d7b33f91dfcce2f87ab7480201d83a6e6a
Aaron Adel <aadel112@gmail.com>
2016-11-25 17:48:55 +0000

    got read globbing working

--- test/test.pl
+++ test/test.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-use Alicia;
+do 'Alicia';
 use Data::Dumper;
 
 $lrc = 0;
@@ -18,6 +18,7 @@ sub main {
     $rc += $rc + test_functions();
     $rc += $rc + test_json(); # using base json
     $rc += $rc + test_script1();
+    $rc += $rc + test_globbing_and_csv_delims();
 #     $rc += $rc + test_os();
 #     $rc += $rc + test_extended_agg();
     if( not $rc ) {
@@ -66,56 +67,13 @@ sub test_file_io{
 }
 
 sub test_functions{
-#     $f = 'examples/func.sql';
-#     create_funfile($f);
-# 
-#     $sql = "SELECT add_one(4)";
-#     $r = $a->exec($sql);
-# #     print Dumper($r);
-#     assert($r->[0][0] == 5, "add_one func", __LINE__);
-# 
-# #     $f = 'examples/fibonacci.sql';
-# #     create_funfile($f);
-# # 
-# #     $sql = "SELECT fibonacci(10)";
-# #     $r = $a->exec($sql);
-# #     print Dumper($r);
-# #     assert($r->[0][0] == 55, "fibonacci func", __LINE__);
-# 
-#     $f = 'examples/funs.pl';
-#     create_funfile_pl($f);
-# 
-#     $sql = "SELECT trim(f1) from it";
-#     $r = $a->exec($sql);
-#     assert($r->[0][0] eq "0 1", "pl func 1", __LINE__);
-# 
-#     $sql = "SELECT fibonacci(10)";
-#     $r = $a->exec($sql);
-# #     print Dumper($r);
-#     assert($r->[0][0] == 55, "fibonacci func", __LINE__);
-#     
+
     $sql = "SELECT LOWER('AAA')";
     $r = $a->exec($sql);
     assert($r->[0][0] eq 'aaa', "C functions work", __LINE__);
 
     $sql = "SELECT SLEFT( 'aaccc', 2 )";
     $r = $a->exec($sql);
-#     print Dumper($r);
-
-#     $sql = "SELECT fibonacci(30)";
-#     $r = $a->exec($sql);
-#     print $r->[0][0] ."\n";
-
-#     $sql = "SELECT ls()";
-#     $r = $a->exec($sql);
-#     print Dumper($r);
-#     print $r->[0][0] ."\n";
-
-#     $sql = "SELECT t()";
-#     $r = $a->exec($sql);
-#     print $r->[0][0] ."\n";
-
-
     $a->drop('it');
     
     return $lrc;
@@ -140,6 +98,15 @@ sub test_script1 {
 
 
     return $lrc;
+}
+
+sub test_globbing_and_csv_delims {
+    my $gfile = 'examples/glob.asql';
+    my $o = $a->parse_and_execute_statements($gfile);
+    my $wc = `cat out/etcout | grep '/var/' | wc -l`;
+    chomp $wc;
+
+    assert($wc > 0, "globbing and delims", __LINE__);
 }
 
 sub test_os {
@@ -167,63 +134,11 @@ sub create_funfile {
 
 sub o {
     my ( $sql ) = @_;
-
-#     print "$sql\n";
     $r = $a->exec($sql);
-   
-#     print Dumper($r);
-
     return $r->[0][0];
 }
 
 sub test_json{
-    # https://www.postgresql.org/docs/9.3/static/functions-json.html
-    # this is where I'm going ti try to pick up inspiration
-
-    #going with leffer's version 
-    #https://www.sqlite.org/src/artifact/e0797ef1bfa63742
-
-    #pointless w/o arrays
-#     $r = o("SELECT array_to_json('{{1,5},{99,100}}')");
-#     assert($r eq '[[1,5],[99,100]]', "array_to_json", __LINE__);
-#     
-    
-#     $r = o("SELECT row_to_json(row(1,'foo'))");
-#     assert($r eq '{"f1":1,"f2":"foo"}', 'row_to_json', __LINE__);
-# 
-#     $r = o('SELECT to_json(\'Fred said "Hi."\'::text)');
-#     assert($r eq "Fred said \"Hi.\"",'to_json',__LINE__);
-# 
-#     $r = o('SELECT json_array_length(\'[1,2,3,{"f1":1,"f2":[5,6]},4]\')');
-#     assert($r == 5, 'json_array_length', __LINE__);
-
-#     $r = o('select key from json_each(\'{"a":"foo", "b":"bar"}\')'); 
-#     assert($r eq 'a', 'json_each', __LINE__);
-# 
-#     $r = o('SELECT json_extract_path(\'{"f2":{"f3":1},"f4":{"f5":99,"f6":"foo"}}\',\'f4\', \'f6\'');
-#     assert($r eq '{"f5":99,"f6":"foo"}', 'json_extract_path_text', __LINE__);
-# 
-#     $r = o('json_object_keys(\'{"f1":"abc","f2":{"f3":"a", "f4":"b"}}\')');
-#     assert($r eq 'f1', 'json_object_keys', __LINE__);
-# 
-#     $r = o('select a from json_populate_record(null::x, \'{"a":1,"b":2}\')');
-#     assert($r == 1, 'json_populate_record', __LINE__);
-# 
-#     $r = o('select a from json_populate_recordset(null::x, \'[{"a":1,"b":2},{"a":3,"b":4}]\')');
-#     assert($r == 1, 'json_populate_recordset', __LINE__);
-# 
-#     $r = o('select * from json_array_elements(\'[1,true, [2,false]]\')');
-#     assert($r, 'json_array_elements', __LINE__);
-# 
-#     $r = o('select JSON(\'[1,2,3]\', 2)');
-#     assert($r == 3, 'json array element', __LINE__);
-# 
-#     $r = o('select JSON(\'{"a":1,"b":2}\', b)');
-#     assert($r == 2, 'json object field', __LINE__);
-# 
-#     $r = o('select JSON(\'{"a":[1,2,3],"b":[4,5,6]}\', \'{a, 2}\'');
-#     assert($r == 3, 'json object path', __LINE__);
-
     $r = o('select json_object(\'ex\',\'[52,3.14159]\')');
     assert($r eq '{"ex":"[52,3.14159]"}', 
         "json loaded", __LINE__);
